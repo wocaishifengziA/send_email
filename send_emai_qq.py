ef5bd0a4ee3b2ca2f6b889b0695b52a5ed05ef3b
dengxiaoyang <dengxiaoyang@jiangxing.ai>
2019-07-08 20:35:25 +0800

    1

---
+++ send_emai_qq.py
@@ -0,0 +1,48 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+settings = {}
+
+
+def setup(mail_host, mail_user, mail_pass):
+    global settings
+    settings = {
+        "mail_host": mail_host,
+        "mail_user": mail_user,
+        "mail_pass": mail_pass
+    }
+
+
+def send_email_notify(recv_addr, topic, message):
+    """
+    发送邮件， QQ版本
+    :param recv_addr: 接收邮箱
+    :param message:   发送内容 {"topic":"", "content":""}
+    :return:
+    """
+
+    # 第三方 SMTP 服务 QQ
+    mail_host = settings["mail_host"]  # 设置服务器
+    mail_user = settings["mail_user"]  # 用户名
+    mail_pass = settings["mail_pass"]  # 口令
+
+    sender = mail_user       # 发送邮箱
+    receivers = [recv_addr]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    message = MIMEText(message, 'plain', 'utf-8')
+    message['From'] = Header("", 'utf-8')
+    message['To'] = Header(recv_addr, 'utf-8')
+
+    subject = topic
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
+
